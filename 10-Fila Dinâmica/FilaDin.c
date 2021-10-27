fcfe01dbae112aa62412d9b2d3a41cc8113cbc44
lucasivisson <lucasivisson1@gmail.com>
2021-10-26 22:42:16 -0300

    finalizando fila dinamica

--- 10-Fila Dinâmica/FilaDin.c
+++ 10-Fila Dinâmica/FilaDin.c
@@ -30,3 +30,59 @@ void libera_fila(Fila *fi) {
         free(fi);
     }
 }
+
+int tamanho_fila(Fila *fi) {
+    if(fi == NULL) return 0;
+    int cont = 0;
+    Elem *no = fi->inicio;
+    while(no != NULL) {
+        cont++;
+        no = no->prox;
+    }
+    return cont;
+}
+
+int fila_cheia(Fila *fi) {
+    return 0;
+}
+
+int fila_vazia(Fila *fi) {
+    if(fi == NULL) return 1;
+    if(fi->inicio == NULL)
+        return 1;
+    return 0;
+}
+
+int insere_fila(Fila *fi, struct aluno al) {
+    if(fi == NULL) return 0;
+    Elem *no = (Elem *) malloc(sizeof(Elem));
+    if(no == NULL) return 0;
+    no->dados = al;
+    no->prox = NULL;
+    if(fi->final == NULL) //fila vazia
+        fi->inicio = no;
+    else
+        fi->final->prox = no;
+    fi->final = no;
+    return 1;
+}
+
+int remove_fila(Fila *fi) {
+    if(fi == NULL) return 0;
+    if(fi->inicio == NULL) //fila vazia
+        return 0;
+    Elem *no = fi->inicio;
+    fi->inicio = fi->inicio->prox;
+    if(fi->inicio == NULL)//fila ficou vazia
+        fi->final = NULL;
+    free(no);
+    return 1;
+}
+
+int consulta_fila(Fila *fi, struct aluno *al) {
+    if(fi == NULL) return 0;
+    if(fi->inicio == NULL) //fila vazia
+        return 0;
+    *al = fi->inicio->dados;
+    return 1;
+}
